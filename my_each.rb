f4f31009581c046fe479a354073f25c6886b4c4f
Javier <joker07ln@gmail.com>
2021-01-27 13:28:34 -0500

    commit tree 6bbba7985ceb0898280fd5c84f9aedc75531dc00

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,23 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+end
+
+
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    binding.pry
+    new_array << yield(array[i])
+    binding.pry
+    i += 1
+  end
+  new_array
 end
